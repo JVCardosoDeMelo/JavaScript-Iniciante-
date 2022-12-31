6229b6185911c509c6ca4930c865bed414d280e6
unknown <sr.melovitor@gnail.com>
2022-12-30 22:35:42 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,8 @@
+// tipo de dado
+//booleanos 
+
+const numero= 456;
+const texto = "456";
+
+console.log(numero + Number(texto))
+
---
+++ trulhy-false.js
@@ -0,0 +1,29 @@
+// booelean
+
+const usuariologado = true
+const contapaga = false
+
+// truhly ou falsy
+
+// 0 --> false
+// 1 --> true
+
+console.log(0 == false)
+console.log("" == false)
+console.log( 1 == true)
+
+
+// undefined
+//null ==> vazio ou nada
+
+let minhavar;
+let varnull = null;
+
+console.log(minhavar)
+console.log(varnull)
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhavar)
+console.log(typeof varnull)
