7d294a385a1587cf5fb402884019c7ae0b96c81b
unknown <sr.melovitor@gnail.com>
2023-01-16 10:26:16 -0300

    Conceitos comparação e template

---
+++ template-string.js
@@ -0,0 +1,8 @@
+const nome= "ju";
+const idade = 2021-1981;
+const cidadedenascimento = "São paulo";
+
+//const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadedenascimento;
+const apresentacao =  `meu nome é ${nome} , minha idade é ${idade}, e moro em ${cidadedenascimento} `;
+
+console.log(apresentacao)
---
+++ ternario.js
@@ -0,0 +1,10 @@
+const idadeMinima =  18;
+const idadecliente = 19;
+
+//if( idadecliente >= idadeMinima ) {
+//console.log("cerveja")
+//} else {
+//    console.log("suco")
+//}
+
+console.log(idadecliente >= idadeMinima ? "cerveja" : "suco")
